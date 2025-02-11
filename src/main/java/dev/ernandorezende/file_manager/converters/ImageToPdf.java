8fb46a43164b2114f5e1e852d5fd2e91f5e8ce83
Ernando Rezende <ernando.03@gmail.com>
2025-02-11 23:46:28 +0000

    Implemented class to convert image to pdf

---
+++ src/main/java/dev/ernandorezende/file_manager/converters/ImageToPdf.java
@@ -0,0 +1,35 @@
+package dev.ernandorezende.file_manager.converters;
+
+import com.itextpdf.text.Document;
+import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.Image;
+import com.itextpdf.text.PageSize;
+import com.itextpdf.text.pdf.PdfName;
+import com.itextpdf.text.pdf.PdfWriter;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class ImageToPdf {
+
+    public  void convert(String tempDir, String fileName, String fileType) throws IOException, DocumentException {
+        File root = new File(tempDir);
+        if (!root.exists()) {
+            root.mkdirs();
+        }
+        String outputFile = fileName + ".pdf";
+
+        Document document = new Document();
+        PdfWriter.getInstance(document, new FileOutputStream(new File(root, outputFile)));
+        document.open();
+
+        document.newPage();
+        Image image = Image.getInstance(new File(root,fileName+fileType).getAbsolutePath());
+        image.setAbsolutePosition(0, 0);
+        image.setBorderWidth(0);
+        image.scaleAbsolute(PageSize.A4);
+        document.add(image);
+        document.close();
+    }
+}
