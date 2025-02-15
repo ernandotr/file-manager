8a84b160358a426906c733810731a2d3583a7876
Ernando Rezende <ernando.03@gmail.com>
2025-02-15 00:25:35 +0000

    Adjusted test for  WaterMarkPdf class

--- src/test/java/dev/ernandorezende/file_manager/pdf/WaterMaeksPdfTest.java
+++ src/test/java/dev/ernandorezende/file_manager/pdf/WaterMaeksPdfTest.java
@@ -1,20 +1,39 @@
 package dev.ernandorezende.file_manager.pdf;
 
+import com.itextpdf.kernel.pdf.PdfDocument;
+import com.itextpdf.kernel.pdf.PdfWriter;
+import com.itextpdf.kernel.pdf.PdfReader;
+import com.itextpdf.kernel.pdf.extgstate.PdfExtGState;
+import com.itextpdf.layout.Document;
+import com.itextpdf.layout.element.Paragraph;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.io.File;
+import java.io.IOException;
 
 public class WaterMaeksPdfTest {
 
     @Test
-    void givenAPDFFile_thenAddWaterMarkSuccess() {
-        File output = new File("src/test/resources/watermarks.pdf");
-        File input = new File("src/test/resources/input-files/contrato.pdf");
+    void givenAPDFFile_thenAddWaterMarkSuccess() throws IOException {
+        String out = "src/test/resources/watermarks.pdf";
+        String input = "src/test/resources/input-files/contrato.pdf";
 
+        String watermark = "CONFIDENTIAL";
         WaterMarkPdf waterMarkPdf = new WaterMarkPdf();
-        waterMarkPdf.addWaterMark(input, output);
-        Assertions.assertTrue(output.exists());
+
+        try(PdfDocument pdfDocument = new PdfDocument(new PdfReader(input), new PdfWriter(out))) {
+            Document document = new Document(pdfDocument);
+            Paragraph paragraph = waterMarkPdf.createWatermarkParagraph(watermark, 0.1f, 56);
+            PdfExtGState extGState = new PdfExtGState().setFillOpacity(0.5f);
+            for (int i = 0; i < document.getPdfDocument().getNumberOfPages(); i++) {
+                waterMarkPdf.addWatermarkToExistingPDF(document, i+1, paragraph, extGState, 0f);
+            }
+
+        }
+
+        File file = new File(out);
+        Assertions.assertTrue(file.exists());
 
     }
 }
